e164d6550022b98b057cb4582a932240adc55a8b
Agus Garcia <agusss.garcia19@gmail.com>
2026-09-09 06:15:32 -0300

    feat(NodesTracker): add utility

---
+++ ECS.Utils/NodesTracker.TNode.cs
@@ -0,0 +1,89 @@
+using Godot;
+
+namespace ECS.Utils;
+
+/// <summary>
+/// Watches a Godot node subtree and maintains a live set of descendant nodes of type <typeparamref name="TNode"/>.
+/// </summary>
+public sealed class NodesTracker<TNode>
+    where TNode : Node
+{
+    /// <summary>
+    /// Raised when a node of type <typeparamref name="TNode"/> enters the tracked subtree.
+    /// </summary>
+    public event Action<TNode>? NodeTracked;
+
+    /// <summary>
+    /// Raised when a node of type <typeparamref name="TNode"/> exits the tracked subtree.
+    /// </summary>
+    public event Action<TNode>? NodeUntracked;
+
+    /// <summary>
+    /// Live set of all nodes of type <typeparamref name="TNode"/> currently in the tracked subtree.
+    /// </summary>
+    public IReadOnlySet<TNode> Nodes => this._nodes;
+
+    private readonly HashSet<TNode> _nodes = [];
+
+    private Node? _root;
+
+    /// <summary>
+    /// Starts watching <paramref name="root"/> and all its descendants; the root itself is excluded from <see cref="Nodes"/>.
+    /// </summary>
+    public void Track(Node root)
+    {
+        if (this._root != null)
+            throw new InvalidOperationException($"You need to call '{nameof(this.Untrack)}' before calling '{nameof(this.Track)}'");
+
+        this._root = root;
+
+        this.OnChildEnteredTree(this._root);
+    }
+
+    private void OnChildEnteredTree(Node node)
+    {
+        node.ChildEnteredTree += this.OnChildEnteredTree;
+        node.ChildExitingTree += this.OnChildExitingTree;
+
+        if (node != this._root && node is TNode match)
+        {
+            if (!this._nodes.Add(match))
+                throw new InvalidOperationException($"Node {match.Name} has already being tracked");
+
+            this.NodeTracked?.Invoke(match);
+        }
+
+        foreach (var child in node.GetChildren())
+            this.OnChildEnteredTree(child);
+    }
+
+    private void OnChildExitingTree(Node node)
+    {
+        foreach (var child in node.GetChildren())
+            this.OnChildExitingTree(child);
+
+        if (node != this._root && node is TNode match)
+        {
+            if (!this._nodes.Remove(match))
+                throw new InvalidOperationException($"Node {match.Name} hasn't been tracked before");
+
+            this.NodeUntracked?.Invoke(match);
+        }
+
+        node.ChildExitingTree -= this.OnChildExitingTree;
+        node.ChildEnteredTree -= this.OnChildEnteredTree;
+    }
+
+    /// <summary>
+    /// Stops watching the subtree and cleans up all event subscriptions.
+    /// </summary>
+    public void Untrack()
+    {
+        if (this._root == null)
+            throw new InvalidOperationException($"You need to call '{nameof(this.Track)}' before calling '{nameof(this.Untrack)}'");
+
+        this.OnChildExitingTree(this._root);
+
+        this._root = null;
+    }
+}
