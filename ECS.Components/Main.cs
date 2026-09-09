3c56f1c1b3e8b8790306f7a71602d48ddb42f198
Agus Garcia <agusss.garcia19@gmail.com>
2026-09-09 06:16:48 -0300

    feat(Main): add component

---
+++ ECS.Components/Main.cs
@@ -0,0 +1,12 @@
+using ECS.Core;
+using Godot;
+
+namespace ECS.Components;
+
+/// <summary>
+/// Marker component used to tag an entity as the primary entity of its scene.
+/// </summary>
+[GlobalClass]
+public partial class Main : Component
+{
+}
