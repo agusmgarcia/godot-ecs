f4938dffd8ab866122c2af4284c7fb9649cee10b
Agus Garcia <agusss.garcia19@gmail.com>
2026-09-11 17:28:58 -0300

    feat(Velocity): integrate with FloorDetector

--- ECS/Components/Velocity.cs
+++ ECS/Components/Velocity.cs
@@ -1,4 +1,5 @@
 using ECS.Core;
+using ECS.Interfaces;
 using Godot;
 
 namespace ECS.Components;
@@ -29,28 +30,16 @@ public partial class Velocity : Component<Vector3>
     [Export(PropertyHint.Range, "0,100,or_greater,hide_control,suffix:m/s")]
     public float MaxSpeed { get; protected set; }
 
+    private FloorDetector? _floorDetector;
+    private Vector3 _direction;
+    private float _speed;
+    private float _pendingSpeedDelta;
+
     /// <summary>
     /// Initialises the component with zero initial velocity.
     /// </summary>
     public Velocity()
         : base(Vector3.Zero) { }
-
-    private Vector3 _direction;
-    private float _speed;
-    private float _pendingSpeedDelta;
-
-    /// <inheritdoc/>
-    protected override void OnInit()
-    {
-        base.OnInit();
-
-        this._direction = Vector3.Zero;
-        this._speed = 0;
-        this._pendingSpeedDelta = 0;
-
-        base.ValueChanged += this.OnVelocityChanged;
-        this.OnVelocityChanged(Vector3.Zero);
-    }
 
     /// <summary>
     /// Applies an acceleration impulse in the given direction this physics frame.
@@ -68,23 +57,42 @@ public partial class Velocity : Component<Vector3>
         this._pendingSpeedDelta = -deceleration;
 
     /// <inheritdoc/>
+    protected override void OnInit()
+    {
+        base.OnInit();
+
+        this._floorDetector = null;
+        this._direction = Vector3.Zero;
+        this._speed = 0;
+        this._pendingSpeedDelta = 0;
+
+        base.ValueChanged += this.OnVelocityChanged;
+        this.OnVelocityChanged((base.Entity as CharacterBody3D)?.Velocity ?? Vector3.Zero);
+    }
+
+    /// <inheritdoc/>
+    protected override void OnSiblingTracked(IComponent component)
+    {
+        base.OnSiblingTracked(component);
+
+        if (component is FloorDetector floorDetector && this._floorDetector == null)
+            this._floorDetector = floorDetector;
+    }
+
+    /// <inheritdoc/>
     protected override void OnUpdate(double delta)
     {
         base.OnUpdate(delta);
-
-        // TODO: check if it makes sense to have a component that tracks whether an entity is on the floor or not.
-        // if not, the casting should be optional base.Entity! as CharacterBody3D.
-        var body = (CharacterBody3D)base.Entity!;
 
         this._speed += this._pendingSpeedDelta * (float)delta;
         this._speed = Mathf.Clamp(this._speed, 0f, this.MaxSpeed);
         this._pendingSpeedDelta = 0f;
 
-        if (!body.IsOnFloor())
+        if (!(this._floorDetector?.Value ?? false))
             this._speed = Mathf.Max(this._speed - this.AirFriction * (float)delta, 0f);
 
         var velocity = this._direction * this._speed;
-        velocity.Y = body.IsOnFloor()
+        velocity.Y = (this._floorDetector?.Value ?? false)
             ? (velocity.Y <= 0 ? 0 : velocity.Y)
             : (base.Value.Y - this.Gravity * (float)delta);
 
@@ -93,20 +101,30 @@ public partial class Velocity : Component<Vector3>
 
     private void OnVelocityChanged(Vector3 velocity)
     {
-        var characterBody3D = base.Entity! as CharacterBody3D;
+        var characterBody3D = base.Entity as CharacterBody3D;
         characterBody3D?.Velocity = velocity;
         characterBody3D?.MoveAndSlide();
     }
 
     /// <inheritdoc/>
+    protected override void OnSiblingUntracked(IComponent component)
+    {
+        if (component is FloorDetector floorDetector && this._floorDetector == floorDetector)
+            this._floorDetector = null;
+
+        base.OnSiblingUntracked(component);
+    }
+
+    /// <inheritdoc/>
     protected override void OnDispose()
     {
-        this.OnVelocityChanged(Vector3.Zero);
+        this.OnVelocityChanged((base.Entity as CharacterBody3D)?.Velocity ?? Vector3.Zero);
         base.ValueChanged -= this.OnVelocityChanged;
 
         this._pendingSpeedDelta = 0;
         this._speed = 0;
         this._direction = Vector3.Zero;
+        this._floorDetector = null;
 
         base.OnDispose();
     }
