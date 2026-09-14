ba4e70523f025d5686abce94a40080f69a549fca
Agus Garcia <agusss.garcia19@gmail.com>
2026-09-13 22:13:10 -0300

    feat(AnimationPlayer): expose GetAnimationList

--- ECS/Components/AnimationPlayer.cs
+++ ECS/Components/AnimationPlayer.cs
@@ -7,7 +7,7 @@ namespace ECS.Components;
 /// ECS-aware wrapper around <see cref="Godot.AnimationPlayer"/> that implements <see cref="IComponent"/>.
 /// </summary>
 [GlobalClass]
-[HideInheritedMembers("Name", "Play")]
+[HideInheritedMembers("GetAnimationList", "Name", "Play")]
 public partial class AnimationPlayer : Godot.AnimationPlayer, IComponent
 {
     /// <summary>
