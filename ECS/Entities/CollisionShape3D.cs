6fd76cbd67932e09993f734b2337200b648df1e1
Agus Garcia <agusss.garcia19@gmail.com>
2026-09-13 22:25:39 -0300

    feat(CollisionShape3D)!: convert it into an entity

---
+++ ECS/Entities/CollisionShape3D.cs
@@ -0,0 +1,13 @@
+using ECS.Interfaces;
+using Godot;
+
+namespace ECS.Entities;
+
+/// <summary>
+/// ECS-aware wrapper around <see cref="Godot.CollisionShape3D"/> that implements <see cref="IEntity"/>.
+/// </summary>
+[GlobalClass]
+[HideInheritedMembers("Name", "SceneFilePath")]
+public partial class CollisionShape3D : Godot.CollisionShape3D, IEntity
+{
+}
