86cb2f2552acccf11fbbd963b96b7187bacb9edf
Agus Garcia <agusss.garcia19@gmail.com>
2026-09-13 21:33:13 -0300

    feat(AnimationPlayer): modelate current animation as value

--- ECS/Components/AnimationPlayer.cs
+++ ECS/Components/AnimationPlayer.cs
@@ -7,8 +7,63 @@ namespace ECS.Components;
 /// ECS-aware wrapper around <see cref="Godot.AnimationPlayer"/> that implements <see cref="IComponent"/>.
 /// </summary>
 [GlobalClass]
-[HideInheritedMembers("Name")]
+[HideInheritedMembers("Name", "Play")]
 public partial class AnimationPlayer : Godot.AnimationPlayer, IComponent
 {
     string INode.Name => base.Name;
+
+    /// <summary>
+    /// Raised when <see cref="Value"/> is assigned a value that differs from the current one.
+    /// </summary>
+    public event Action<StringName?>? ValueChanged;
+
+    /// <summary>
+    /// The current value.
+    /// </summary>
+    public StringName? Value
+    {
+        get;
+        protected set
+        {
+            if (EqualityComparer<StringName>.Default.Equals(field, value))
+                return;
+
+            field = value;
+            this.ValueChanged?.Invoke(value);
+        }
+    }
+
+    /// <summary>
+    /// Creates a new animation player
+    /// </summary>
+    public AnimationPlayer() =>
+        this.Value = base.CurrentAnimation;
+
+    /// <summary>
+    /// Called once after the component enters the scene tree and internal state is ready.
+    /// </summary>
+    protected virtual void OnInit()
+    {
+        base.AnimationStarted += this.OnAnimationStarted;
+        base.AnimationFinished += this.OnAnimationFinished;
+        this.OnAnimationStarted(base.CurrentAnimation);
+        base.Stop();
+    }
+
+    private void OnAnimationStarted(StringName animationName) =>
+        this.Value = animationName;
+
+    private void OnAnimationFinished(StringName animationName) =>
+        this.Value = this.Value == animationName ? null : this.Value;
+
+    /// <summary>
+    /// Called once before the component exits the scene tree and internal state is torn down.
+    /// </summary>
+    protected virtual void OnDispose()
+    {
+        base.Stop();
+        this.OnAnimationFinished(base.CurrentAnimation);
+        base.AnimationFinished -= this.OnAnimationFinished;
+        base.AnimationStarted -= this.OnAnimationStarted;
+    }
 }
