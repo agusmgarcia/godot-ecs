43ad7e624919a35c0c65ef8b3dcb51f045be6a6a
Agus Garcia <agusss.garcia19@gmail.com>
2026-09-13 23:35:35 -0300

    feat(State): track reference to the state machine parent

--- ECS/Components/State.cs
+++ ECS/Components/State.cs
@@ -1,4 +1,5 @@
 using ECS.Core;
+using ECS.Entities;
 using Godot;
 
 namespace ECS.Components;
@@ -10,9 +11,30 @@ namespace ECS.Components;
 public partial class State : Component
 {
     /// <summary>
+    /// The current state machine parent.
+    /// </summary>
+    public StatesMachine? Parent { get; private set; }
+
+    /// <inheritdoc/>
+    protected override void OnInit()
+    {
+        base.OnInit();
+
+        this.Parent = base.GetParentOrNull<StatesMachine>();
+    }
+
+    /// <summary>
     /// Returns <c>false</c> to block non-forced transitions away from this state.
     /// </summary>
     internal protected virtual bool ReadyToTransition() => true;
 
     internal virtual void CopyStateParams(ValueType stateParams) { }
+
+    /// <inheritdoc/>
+    protected override void OnDispose()
+    {
+        this.Parent = null;
+
+        base.OnDispose();
+    }
 }
